451e884862c89f711e7d0c26e788834dc4c787ea
Henrik Gudmundsson <hg222dt@student.lnu.se>
2015-03-24 17:09:33 +0100

    Added text help annotation for where to put client key

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,9 @@
 angular.module('positioning_service_client_app')
     .constant('API', {
-        'key': "b73c7bd31eda0f56af490a146fd589d4",
+
+        'key': "<- Place your api client key here ->",
+        
         'url': "http://localhost:3000/api/v1/",
+        
         'format': 'application/json'
 });
