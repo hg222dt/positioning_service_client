42f0de1d786eb790e43517d12a144f56ad4b72de
Henrik Gudmundsson <hg222dt@student.lnu.se>
2015-03-24 16:46:10 +0100

    Created config file

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+angular.module('positioning_service_client_app')
+    .constant('API', {
+        'key': "b73c7bd31eda0f56af490a146fd589d4",
+        'url': "http://localhost:3000/api/v1/",
+        'format': 'application/json'
+});
